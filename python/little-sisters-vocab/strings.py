38a6756d9496732b9e3cee6a3e4bba0aa6d75b81
Sougata Khan <sougat818@gmail.com>
2022-02-05 14:06:38 +1100

    Little Sisters Vocab (Python)

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -39,7 +39,7 @@ def remove_suffix_ness(word):
     return word[:-4] if word[-5] != 'i' else word[:-5]+'y'
 
 
-def noun_to_verb(sentence, index):
+def adjective_to_verb(sentence, index):
     """
 
     :param sentence: str that uses the word in sentence
