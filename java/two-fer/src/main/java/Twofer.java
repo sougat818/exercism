91c17f38fe9c1e0a433c5564047c8268c3dae87f
Sougata Khan <sougat818@gmail.com>
2022-02-05 14:45:01 +1100

    Two Fer (Java)

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -2,9 +2,7 @@ import static java.util.Objects.isNull;
 
 public class Twofer {
     public String twofer(String name) {
-        if(isNull(name)){
-            name = "you";;
-        }
+        name = isNull(name) ? "you" : name;
         return "One for " + name + ", one for me.";
     }
 }
