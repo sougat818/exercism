a07dc015575db4758ead465d161ed8448f76ddce
Sougata Khan <sougat818@gmail.com>
2023-11-04 05:22:03 +0000

    java:logLevels fix spotbugs error

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class LogLevels {
 
     public static String message(String logLine) {
@@ -7,7 +9,7 @@ public class LogLevels {
     public static String logLevel(String logLine) {
         return logLine.split("]: ")[0]
                 .substring(1)
-                .toLowerCase();
+                .toLowerCase(Locale.ROOT);
     }
 
     public static String reformat(String logLine) {
